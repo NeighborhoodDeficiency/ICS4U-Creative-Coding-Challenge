ed2bc5d6e4030e813a8e57d72943799944ee46e3
lrozi1 <47332155+lrozi1@users.noreply.github.com>
2021-03-11 12:13:47 -0500

    Update sketch.js
    Copied from p5js

--- sketch.js
+++ sketch.js
@@ -1,1 +1,18 @@
+const flock = [];
 
+function setup() {
+  createCanvas(1600, 900);
+  for (let i=0; i < 100; i++) {
+  flock.push(new Boid());
+}
+}
+
+function draw() {
+  background(40);
+  for (let boid of flock) {
+    boid.edges();
+    boid.flock(flock);
+    boid.update();
+    boid.show();
+  }
+}
